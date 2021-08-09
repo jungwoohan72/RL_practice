8df64bbafbc399226d42af8a975d96840cd7570f
Jungwoo <jungwoohan72@kaist.ac.kr>
2021-08-09 22:35:53 +0900

    REINFORCE implementation

--- REINFORCE/train.py
+++ REINFORCE/train.py
@@ -8,6 +8,7 @@ from MLP import MLP as MLP
 from REINFORCE import REINFORCE as REINFORCE
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(DEVICE)
 GAMMA = 0.9
 lr = 0.0002
 n_eps = 10000
@@ -18,10 +19,12 @@ s_dim = env.observation_space.shape[0]
 a_dim = env.action_space.n
 
 net = MLP(s_dim, a_dim, [128]).to(DEVICE)
-agent = REINFORCE(net, GAMMA, lr).to(DEVICE)
+agent = REINFORCE(net, GAMMA, lr, DEVICE).to(DEVICE)
 
 for ep in range(n_eps):
     s = env.reset()
+    s = torch.from_numpy(s).float().to(DEVICE)
+    s = s.view((1,4))
     cum_r = 0
 
     states = []
@@ -29,6 +32,7 @@ for ep in range(n_eps):
     rewards = []
 
     while True:
+        env.render()
         a = agent.get_action(s)
         ns, r, done, info = env.step(a.item())
 
@@ -40,17 +44,14 @@ for ep in range(n_eps):
         if done:
             break
 
-    if ep%print_every == 0:
+    if ep % print_every == 0:
         print("Episode: {} || Cumulative Reward: {}".format(ep, cum_r))
 
-
-    print(states.shape)
-    print(actions.shape)
-    print(rewards.shape)
     states = torch.cat(states, dim = 0)
     rewards = torch.tensor(rewards)
 
     actions = torch.stack(actions).squeeze()
+
     episode = (states, actions, rewards)
-    agent.update_episode()
+    agent.update_episode(episode)
 
