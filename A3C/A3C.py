8195d26650c2176b77a479c4ea1ece3c98af81b2
Jungwoo <jungwoohan72@kaist.ac.kr>
2021-08-19 16:59:45 +0900

    A3C

--- A3C/A3C.py
+++ A3C/A3C.py
@@ -1,144 +1,132 @@
-"""
-Reinforcement Learning (A3C) using Pytroch + multiprocessing.
-The most simple implementation for continuous action.
-View more on my Chinese tutorial page [莫烦Python](https://morvanzhou.github.io/).
-"""
-
+import gym
 import torch
 import torch.nn as nn
-from utils import v_wrap, set_init, push_and_pull, record
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
 import torch.multiprocessing as mp
-from shared_adam import SharedAdam
-import gym
-import os
+import time
 
-os.environ["OMP_NUM_THREADS"] = "1"
-
-UPDATE_GLOBAL_ITER = 5
-GAMMA = 0.9
-MAX_EP = 3000
-
-env = gym.make('CartPole-v0')
-N_S = env.observation_space.shape[0]
-N_A = env.action_space.n
-
-class SharedAdam(torch.optim.Adam):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.99), eps=1e-8,
-                 weight_decay=0):
-        super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
-        # State initialization
-        for group in self.param_groups:
-            for p in group['params']:
-                state = self.state[p]
-                state['step'] = 0
-                state['exp_avg'] = torch.zeros_like(p.data)
-                state['exp_avg_sq'] = torch.zeros_like(p.data)
-
-                # share in memory
-                state['exp_avg'].share_memory_()
-                state['exp_avg_sq'].share_memory_()
-
-class Net(nn.Module):
-    def __init__(self, s_dim, a_dim):
-        super(Net, self).__init__()
-        self.s_dim = s_dim
-        self.a_dim = a_dim
-        self.pi1 = nn.Linear(s_dim, 128)
-        self.pi2 = nn.Linear(128, a_dim)
-        self.v1 = nn.Linear(s_dim, 128)
-        self.v2 = nn.Linear(128, 1)
-        set_init([self.pi1, self.pi2, self.v1, self.v2])
-        self.distribution = torch.distributions.Categorical
-
-    def forward(self, x):
-        pi1 = torch.tanh(self.pi1(x))
-        logits = self.pi2(pi1)
-        v1 = torch.tanh(self.v1(x))
-        values = self.v2(v1)
-        return logits, values
-
-    def choose_action(self, s):
-        self.eval()
-        logits, _ = self.forward(s)
-        prob = F.softmax(logits, dim=1).data
-        m = self.distribution(prob)
-        return m.sample().numpy()[0]
-
-    def loss_func(self, s, a, v_t):
-        self.train()
-        logits, values = self.forward(s)
-        td = v_t - values
-        c_loss = td.pow(2)
-
-        probs = F.softmax(logits, dim=1)
-        m = self.distribution(probs)
-        exp_v = m.log_prob(a) * td.detach().squeeze()
-        a_loss = -exp_v
-        total_loss = (c_loss + a_loss).mean()
-        return total_loss
+# Hyperparameters
+n_train_processes = mp.cpu_count()
+learning_rate = 0.0002
+update_interval = 5
+gamma = 0.98
+max_train_ep = 500
+max_test_ep = 520
 
 
-class Worker(mp.Process):
-    def __init__(self, gnet, opt, global_ep, global_ep_r, res_queue, name):
-        super(Worker, self).__init__()
-        self.name = 'w%02i' % name
-        self.g_ep, self.g_ep_r, self.res_queue = global_ep, global_ep_r, res_queue
-        self.gnet, self.opt = gnet, opt
-        self.lnet = Net(N_S, N_A)  # local network
-        self.env = gym.make('CartPole-v0').unwrapped
+class ActorCritic(nn.Module):
+    def __init__(self):
+        super(ActorCritic, self).__init__()
+        self.fc1 = nn.Linear(4, 256)
+        self.fc_pi = nn.Linear(256, 2)
+        self.fc_v = nn.Linear(256, 1)
 
-    def run(self):
-        total_step = 1
-        while self.g_ep.value < MAX_EP:
-            s = self.env.reset()
-            buffer_s, buffer_a, buffer_r = [], [], []
-            ep_r = 0.
-            while True:
-                if self.name == 'w00':
-                    self.env.render()
-                a = self.lnet.choose_action(v_wrap(s[None, :]))
-                s_, r, done, _ = self.env.step(a)
-                if done: r = -1
-                ep_r += r
-                buffer_a.append(a)
-                buffer_s.append(s)
-                buffer_r.append(r)
+    def pi(self, x, softmax_dim=0):
+        x = F.relu(self.fc1(x))
+        x = self.fc_pi(x)
+        prob = F.softmax(x, dim=softmax_dim)
+        return prob
 
-                if total_step % UPDATE_GLOBAL_ITER == 0 or done:  # update global and assign to local net
-                    # sync
-                    push_and_pull(self.opt, self.lnet, self.gnet, done, s_, buffer_s, buffer_a, buffer_r, GAMMA)
-                    buffer_s, buffer_a, buffer_r = [], [], []
-
-                    if done:  # done and print information
-                        record(self.g_ep, self.g_ep_r, ep_r, self.res_queue, self.name)
-                        break
-                s = s_
-                total_step += 1
-        self.res_queue.put(None)
+    def v(self, x):
+        x = F.relu(self.fc1(x))
+        v = self.fc_v(x)
+        return v
 
 
-if __name__ == "__main__":
-    gnet = Net(N_S, N_A)  # global network
-    gnet.share_memory()  # share the global parameters in multiprocessing
-    opt = SharedAdam(gnet.parameters(), lr=1e-4, betas=(0.92, 0.999))  # global optimizer
-    global_ep, global_ep_r, res_queue = mp.Value('i', 0), mp.Value('d', 0.), mp.Queue()
+def train(global_model, rank):
+    local_model = ActorCritic()
+    local_model.load_state_dict(global_model.state_dict())
 
-    # parallel training
-    workers = [Worker(gnet, opt, global_ep, global_ep_r, res_queue, i) for i in range(mp.cpu_count())]
-    [w.start() for w in workers]
-    res = []  # record episode reward to plot
-    while True:
-        r = res_queue.get()
-        if r is not None:
-            res.append(r)
+    optimizer = optim.Adam(global_model.parameters(), lr=learning_rate)
+
+    env = gym.make('CartPole-v1')
+
+    for n_epi in range(max_train_ep):
+        done = False
+        s = env.reset() # s.shape -> (4,)
+        while not done:
+            s_lst, a_lst, r_lst = [], [], []
+            for t in range(update_interval):
+                prob = local_model.pi(torch.from_numpy(s).float()) # torch.size([2]) | torch.from_numpy(s).shape: torch.size([4])
+                m = Categorical(prob)
+                a = m.sample().item() # int
+                s_prime, r, done, info = env.step(a)
+
+                s_lst.append(s)
+                a_lst.append([a])
+                r_lst.append(r/100.0)
+
+                s = s_prime
+                if done:
+                    break
+
+            s_final = torch.tensor(s_prime, dtype=torch.float) # torch.size([4])
+            R = 0.0 if done else local_model.v(s_final).item()
+            td_target_lst = []
+            for reward in r_lst[::-1]:
+                R = gamma * R + reward # n-step TD target
+                td_target_lst.append([R])
+            td_target_lst.reverse()
+
+            s_batch, a_batch, td_target = torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
+                torch.tensor(td_target_lst) # torch.size([update_interval,4]), torch.size([update_interval,1]), torch.size([update_interval,1])
+            advantage = td_target - local_model.v(s_batch)
+
+            pi = local_model.pi(s_batch, softmax_dim=1)
+            pi_a = pi.gather(1, a_batch)
+            loss = -torch.log(pi_a) * advantage.detach() + \
+                F.smooth_l1_loss(local_model.v(s_batch), td_target.detach()) # torch.size([5,1])
+
+            optimizer.zero_grad()
+            loss.mean().backward()
+            for global_param, local_param in zip(global_model.parameters(), local_model.parameters()):
+                global_param._grad = local_param.grad
+            optimizer.step()
+            local_model.load_state_dict(global_model.state_dict())
+
+    env.close()
+    print("Training process {} reached maximum episode.".format(rank))
+
+
+def test(global_model):
+    env = gym.make('CartPole-v1')
+    score = 0.0
+    print_interval = 20
+
+    for n_epi in range(max_test_ep):
+        done = False
+        s = env.reset()
+        while not done:
+            if n_epi > 390:
+                env.render()
+            prob = global_model.pi(torch.from_numpy(s).float())
+            a = Categorical(prob).sample().item()
+            s_prime, r, done, info = env.step(a)
+            s = s_prime
+            score += r
+
+        if n_epi % print_interval == 0 and n_epi != 0:
+            print("# of episode :{}, avg score : {:.1f}".format(
+                n_epi, score/print_interval))
+            score = 0.0
+            time.sleep(1)
+    env.close()
+
+
+if __name__ == '__main__':
+    global_model = ActorCritic()
+    global_model.share_memory()
+
+    processes = []
+    print("Available CPU Count:", n_train_processes)
+    for rank in range(n_train_processes + 1):  # + 1 for test process
+        if rank == 0:
+            p = mp.Process(target=test, args=(global_model,))
         else:
-            break
-    [w.join() for w in workers]
-
-    import matplotlib.pyplot as plt
-
-    plt.plot(res)
-    plt.ylabel('Moving average ep reward')
-    plt.xlabel('Step')
-    plt.show()
+            p = mp.Process(target=train, args=(global_model, rank,))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
